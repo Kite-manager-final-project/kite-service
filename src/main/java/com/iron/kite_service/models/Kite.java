2a9da3d18468c818822b959666dc7894b83c8a5a
Víctor Sanz <vsanzc@alumnos.nebrija.es>
2025-05-22 17:12:34 +0200

    Validaciones implementadas

--- src/main/java/com/iron/kite_service/models/Kite.java
+++ src/main/java/com/iron/kite_service/models/Kite.java
@@ -1,6 +1,10 @@
 package com.iron.kite_service.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NonNull;
@@ -18,11 +22,16 @@ public class Kite {
     private int id;
 
     @NonNull
+    @NotNull(message = "El viento requerido no puede estar vacio")
+    @Min(value = 14, message = "El viento mínimo requerido es 14")
+    @Max(value = 40, message = "El viento máximo permitido es 40")
     private int windRequired;
 
     @NonNull
+    @NotBlank(message = "La ubicación no puede estar vacia")
     private String location;
 
     @NonNull
+    @NotBlank(message = "Tienes que especificar el dueño")
     private String owner;
 }
