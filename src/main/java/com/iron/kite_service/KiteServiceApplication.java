32bc2838227863ea09ebcc126c607abd029b7d77
Víctor Sanz <vsanzc@alumnos.nebrija.es>
2025-05-22 16:54:17 +0200

    anotador feign añadido

--- src/main/java/com/iron/kite_service/KiteServiceApplication.java
+++ src/main/java/com/iron/kite_service/KiteServiceApplication.java
@@ -2,8 +2,10 @@ package com.iron.kite_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class KiteServiceApplication {
 
 	public static void main(String[] args) {
