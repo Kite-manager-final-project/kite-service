574a1a81636ea14d00e616b12c9cddfd69be897b
Víctor Sanz <vsanzc@alumnos.nebrija.es>
2025-05-22 16:49:48 +0200

    commit inicial

---
+++ src/main/java/com/iron/kite_service/KiteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.iron.kite_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiteServiceApplication.class, args);
+	}
+
+}
