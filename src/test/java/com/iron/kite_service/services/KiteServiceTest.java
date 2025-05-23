73b6137204808cc891c848e6d0da3e192f82df11
Víctor Sanz <vsanzc@alumnos.nebrija.es>
2025-05-23 13:20:00 +0200

    Test implementado

---
+++ src/test/java/com/iron/kite_service/services/KiteServiceTest.java
@@ -0,0 +1,45 @@
+package com.iron.kite_service.services;
+
+import com.iron.kite_service.models.Kite;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class KiteServiceTest {
+
+    @Autowired
+    KiteService kiteService;
+
+
+    @Test
+    @DisplayName("Asigno 3 cometas al hombre de la rae, que las va a usar en distintas ubicaciones")
+    void insert3Kites(){
+
+        Kite kite1, kite2, kite3;
+
+        kite1 = new Kite(23, "Madrid", "hombre_de_la_rae");
+
+        kite2 = new Kite(23, "Caraquiz, Uceda (Guadalajara)", "hombre_de_la_rae");
+
+        kite3 = new Kite(23, "Torrevieja (Alicante)", "hombre_de_la_rae");
+
+        Kite savedKite1, savedKite2, savedKite3;
+
+        savedKite1 = kiteService.saveKite(kite1);
+        savedKite2 = kiteService.saveKite(kite2);
+        savedKite3 = kiteService.saveKite(kite3);
+
+        System.out.println("================================");
+        System.out.println("Estas son las cometas que has guardado");
+        System.out.println(savedKite1);
+        System.out.println(savedKite2);
+        System.out.println(savedKite3);
+        System.out.println("================================");
+
+    }
+
+}
