8230227ba6984d3c9918f2da8029eab742484738
Víctor Sanz <vsanzc@alumnos.nebrija.es>
2025-05-23 13:03:59 +0200

    Get, post y delete implementado en el servicio

--- src/main/java/com/iron/kite_service/repositories/KiteRepository.java
+++ src/main/java/com/iron/kite_service/repositories/KiteRepository.java
@@ -2,6 +2,18 @@ package com.iron.kite_service.repositories;
 
 import com.iron.kite_service.models.Kite;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface KiteRepository extends JpaRepository<Kite, Integer> {
+
+
+    List<Kite> findKitesByOwner(String owner);
+
+    List<Kite> findKitesByLocation(String location);
+
+    @Query("SELECT k FROM Kite k WHERE k.owner.username = :ownerName AND k.location = :location")
+    List<Kite> findKitesByOwnerAndLocation(@Param("ownerName") String ownerName, @Param("location") String location);
 }
--- src/main/java/com/iron/kite_service/services/KiteService.java
+++ src/main/java/com/iron/kite_service/services/KiteService.java
@@ -3,16 +3,67 @@ package com.iron.kite_service.services;
 import com.iron.kite_service.models.Kite;
 import com.iron.kite_service.repositories.KiteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class KiteService {
 
+    //todo: implementar put y patch
+
     @Autowired
     private KiteRepository kiteRepository;
+
+    //POST
 
     public Kite saveKite(Kite kite){
         return kiteRepository.save(kite);
     }
+
+    //GET
+
+    public List<Kite> getAllKites(){
+        return kiteRepository.findAll();
+    }
+
+    //todo: mandar un mensaje de una excepción personalizada
+    public ResponseEntity<?> getKiteById(int id){
+        Optional<Kite> foundKite = kiteRepository.findById(id);
+
+        if (foundKite.isPresent())
+            return new ResponseEntity<>(foundKite.get(), HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    public List<Kite> getAllKites(String username, String location){
+
+        if (username != null && location != null)
+            return kiteRepository.findKitesByOwnerAndLocation(username, location);
+
+        if (username != null)
+            return kiteRepository.findKitesByOwner(username);
+
+        if (location != null)
+            return kiteRepository.findKitesByLocation(location);
+
+
+        return kiteRepository.findAll();
+    }
+
+    //DELETE
+
+    public void deleteKite(int id){
+
+        var kiteToDelete = kiteRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        kiteRepository.delete(kiteToDelete);
+    }
+
+
 }
