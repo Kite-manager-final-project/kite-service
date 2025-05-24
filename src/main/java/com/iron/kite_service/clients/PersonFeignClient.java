42b072c9a0884b9d82082f895b89a94f2d5964c0
Víctor Sanz <vsanzc@alumnos.nebrija.es>
2025-05-24 20:12:16 +0200

    Correccion errata

--- src/main/java/com/iron/kite_service/clients/PersonFeignClient.java
+++ src/main/java/com/iron/kite_service/clients/PersonFeignClient.java
@@ -3,10 +3,11 @@ package com.iron.kite_service.clients;
 import com.iron.kite_service.dtos.PersonDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "person-service")
 public interface PersonFeignClient {
 
     @GetMapping("/api/person/{nickName}")
-    PersonDTO getPersonByNickName(String nickName);
+    PersonDTO getPersonByNickName(@PathVariable String nickName);
 }
